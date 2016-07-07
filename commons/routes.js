c19b0c2f78bc12cf4b897763f1677e2ec7c59e8d
Jwaggoner <wiggiddywags@gmail.com>
2016-07-07 19:08:20 -0400

    added iron routes and collection

--- commons/routes.js
+++ commons/routes.js
@@ -4,3 +4,16 @@ Router.configure({
 });
 
 Router.onBeforeAction('loading'); //before every action call show loading template
+
+Router.route("/", {
+  //name is "home"
+  name: "home",
+  //on route / the layout template will be the template named "homeLayout"
+  layoutTemplate: "homeLayout",
+  //on route / template named "home" will be rendered
+  template: "home",
+  //render template travelSearch to search section of the layout template.
+  yieldRegions: {
+    travelSearch: {to: "search"}
+  }
+});
